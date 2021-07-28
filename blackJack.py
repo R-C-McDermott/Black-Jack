f13caa0c5e09be74e7b4994fc70f275ff2d9a302
R-C-McDermott <ryan.c.mcdermott4@gmail.com>
2021-07-28 22:48:23 +0100

    v1.1 update
    New version fixes some previously known bugs and adds a new double down feature to the game, allowing the user to double down if they have the sufficient funds. Still need to add soft and hard hands

--- blackJack.py
+++ blackJack.py
@@ -1,11 +1,19 @@
 '''
- - BLACK JACK v1.0 -
+ - BLACK JACK v1.1 -
 
  By Ryan C. McDermott
 
 *v1.0*
-- Functioning player and dealer (1 player so far)
-- Betting system
+- Functioning player, dealer and table objects (1 player so far).
+- Betting system.
+
+*v1.1*
+- Player now has the choice to double down.
+- Betting system now pays out depending on proper Black Jack odds.
+- Fixed dealer hit bug where an infinite loop would occur at 17.
+
+* TO FIX *
+- Soft and hard hands when considering hands with aces.
 
 '''
 import random # to be used for shuffling
@@ -84,7 +92,7 @@ class Human(Player):
         super().__init__()
         self.name = name
         self.chips = chips
-        #self.bet = 0
+        self.bet = 0
 
     def showChips(self):
         print(f"Remaining balance: {self.chips}")
@@ -96,22 +104,36 @@ class Human(Player):
                 print("You don't have enough chips to place that bet!")
                 self.playBet()
             else:
-                #self.bet += player_bet
+                self.bet += player_bet
                 self.chips -= player_bet
                 return player_bet
         except ValueError:
             print("Invalid input")
 
     def playerHitStand(self, deck):
-        player_choice = str(input("Would you like to hit or stand? (H/S): "))
-        player_choice = player_choice.strip().upper()
+        '''
+        For simplicity, the function checks whether the player has sufficient
+        funds in order to offer a double down choice... If not the function
+        simply asks the user to hit or stand.
+
+        '''
+
+        if len(self.hand) == 2 and self.chips >= self.bet:
+            player_choice = str(input("Would you like to hit, stand or double down? (H/S/D): "))
+            player_choice = player_choice.strip().upper()
+        else:
+            player_choice = str(input("Would you like to hit or stand? (H/S): "))
+            player_choice = player_choice.strip().upper()
         try:
             if player_choice == "H":
                 self.draw(deck, 1)
-                return False
+                return False, player_choice
+            if player_choice == "D":
+                self.draw(deck, 1)
+                return True, player_choice
             if player_choice == "S":
-                return True
-            if player_choice not in ["H", "S"]:
+                return True, player_choice
+            if player_choice not in ["H", "S", "D"]:
                 print("Invalid input - Use H (Hit) or S (Stand)")
                 self.playerHitStand(deck, number_of_cards=1)
         except ValueError:
@@ -128,7 +150,7 @@ class Dealer(Player):
         while True:
             if self.cardValueCount() < 17:
                 self.draw(deck, 1)
-            if self.cardValueCount() > 17:
+            if self.cardValueCount() >= 17:
                 break
 
 
@@ -156,22 +178,42 @@ class Table:
             self.addPlayer()
 
     def playRound(self, dealer, deck):
+
+        # Reset variables for new round
         round_over = False
         player_done = False
         player = self.players[0]
         player.hand = []
+        player.bet = 0
         dealer.hand = []
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+
+        # Deck shuffling, player bet and initial 2 card draw for player and dealer
         print("\nShuffling deck...\n")
         deck.shuffleDeck()
         player.showChips()
-        player_bet = player.playBet()
+        player.bet = player.playBet()
+        print(f"\nPlayer's bet: {player.bet}\n")
+        player.showChips()
         player.draw(deck, 2)
         dealer.draw(deck, 2)
+
+        # Loop for player's turn
         while player_done == False:
             print("\n~~~~~~~~ PLAYER'S HAND ~~~~~~~~\n")
             player.showHand()
             print("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
-            player_done = player.playerHitStand(deck)
+            choice = player.playerHitStand(deck)
+
+            # Check for double down
+            player_done = choice[0]
+            if choice[1] == "D":
+                player.chips -= player.bet
+                player.bet *= 2
+                print(f"\nPlayer's bet: {player.bet}\n")
+                player.showChips()
+            # ~~~~~~~~~~~~~~~~~~~ #
+
             print("\n~~~~~~~~ PLAYER'S HAND ~~~~~~~~\n")
             player.showHand()
             print("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
@@ -181,47 +223,59 @@ class Table:
                 round_over = True
             if player.cardValueCount() == 21:
                 print("Blackjack!")
-                player.chips += int(2.5*player_bet) # 3/2 odds for blackjack win plus original bet
+                player.chips += int(2.5*player.bet) # 3/2 odds for blackjack win plus original bet
                 player_done = True
                 round_over = True
             if player.cardValueCount() < 21:
                 pass
+        # __________________________ #
 
+        # Dealer plays until hand value is greater than 17
         if round_over == False:
             dealer.dealerHit(deck)
             print("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
                   "\n\nDealer's Turn...\n\n"
                   "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
             dealer.showHand()
+
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+
+        # Winning and payout conditions
             if dealer.cardValueCount() > 21:
-                print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+                print("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
                       "\nDealer is burst! Player wins!")
-                player.chips += int(2*player_bet) # 1/1 odds for winning plus original bet
+                player.chips += int(2*player.bet) # 1/1 odds for winning plus original bet
                 round_over = True
             elif dealer.cardValueCount() > player.cardValueCount():
-                print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+                print("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
                       "\nDealer wins!")
                 round_over = True
             elif player.cardValueCount() > dealer.cardValueCount():
-                print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+                print("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
                       "\nPlayer wins!")
-                player.chips += int(2*player_bet) # 1/1 odds for winning plus original bet
+                player.chips += int(2*player.bet) # 1/1 odds for winning plus original bet
                 round_over = True
             elif dealer.cardValueCount() == player.cardValueCount():
-                print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+                print("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
                       "\nIt's a draw!")
-                player.chips += player_bet # return chips to player
+                player.chips += player.bet # return chips to player
                 round_over = True
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 
-        try:
-            play_again = str(input("\nWould you like to play another round? (y/n)\n>"))
-            play_again = play_again.strip().lower()
-            if play_again == "y":
-                self.playRound(deck=Deck(), dealer=Dealer()) # Using fresh deck object
-            if play_again == "n":
-                print("\n~~~ Thank you, for playing! ~~~\n")
-        except ValueError:
-            print
+        # Ask user if they would like to play another round
+        if player.chips == 0:
+            print("\nYou've lost all of your chips!\nBetter luck next time\n")
+            print("\n~~~ Thank you for playing! ~~~\n")
+        else:
+            try:
+                play_again = str(input("\nWould you like to play another round? (y/n)\n>"))
+                play_again = play_again.strip().lower()
+                if play_again == "y":
+                    self.playRound(deck=Deck(), dealer=Dealer()) # Using fresh deck object
+                if play_again == "n":
+                    print("\n~~~ Thank you for playing! ~~~\n")
+            except ValueError:
+                print("Invalid input, please use 'y' (yes) or 'n' (no)")
 
 '''
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -244,14 +298,15 @@ Functions for multiple players ... to be used for later versions
 
 
 def main():
-    # Create table, dealer and deck objects
+    # Create table object
     game = Table()
 
-    # Add player to game and shuffle deck
+    # Add player to game
     game.addPlayer()
 
-    # Game loop
+    # Game loop and initialise dealer and deck objects
     game.playRound(dealer=Dealer(), deck=Deck())
+
 
 if __name__ == '__main__':
     main()
